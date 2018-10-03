1aad4da5f12825f2b4b1677fcbb8ccfdd422bc54
Anandha Sudhan <rasudhan@gmail.com>
2018-10-03 00:09:18 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,7 @@ WIN_COMBINATIONS=[
 
 def won(board)
   WIN_COMBINATIONS.each do |win_c|
-    if board[win_c[0]]=="X" && board[win_c[1]]=="X" && board[win_c[2]]=="X"
+    if board[win_c[0]]==board[win_c[1]] && board[win_c[1]]==board[win_c[2]]
       return win_c
     end
   end
